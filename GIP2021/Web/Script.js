2882fcb0209d8d05612cea6da6e7ed11f06b675f
mdecap <mdecap@LL-H1PQ9X2.ION.local>
2021-03-18 15:20:34 +0100

    nothing changed

--- GIP2021/Web/Script.js
+++ GIP2021/Web/Script.js
@@ -35,9 +35,16 @@ new Vue ({
         },
         openInedpth: function (filepath) {
             console.log("test")
-            document.cookie = "image =" + filepath;
+            setCookie("url", filepath, 20)
         },
     }
 
 })
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
