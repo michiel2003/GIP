12f65f61210a86abed9c85c593a2dc2290feb639
Michiel De Cap <mdecap@ION.local>
2021-03-22 15:27:37 +0100

    test

--- GIP2021/SQLSCRIPTS/ImageDBSQL.sql
+++ GIP2021/SQLSCRIPTS/ImageDBSQL.sql
@@ -1,9 +1,17 @@
-create table Image(
-	id integer unsigned auto_increment primary key,
-	imageURL varchar(255)
-);
+insert  into image (imageurl) values
+("U:/GIP michiel 2021/GIP/GIP2021/SampleImages/BlueSquare.png");
 
-insert into image (imageURL) values
-	("file://ionad1/users/Leerlingen/mdecap/My%20Documents/GIP/GIP2021/SampleImages/BlueSquare.png");
-	
+delete from image where imageurl like "test2";
+
+create table image (id integer not null auto_increment, imageurl varchar(255), primary key (id));
+create table imagetag (imageid integer not null, tagid integer not null);
+create table tags (id integer not null auto_increment, tagname varchar(255), primary key (id));
+alter table imagetag add constraint FKqf48l3tkdklv0vngrui15w4rf foreign key (tagid) references tags (id);
+alter table imagetag add constraint FKkdiyw8ygjwuj4hs53ge2qr74x foreign key (imageid) references image (id);
+
+
 drop table image;
+drop table imagetag;
+drop table tags;
+
+select * from imagetag join tags on tagid where imageid like 1;
